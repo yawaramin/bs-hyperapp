e294608be6ba5737d87ffe21bc6065d0ab808b46
Yawar Amin <yawar.amin@gmail.com>
2017-08-29 00:00:14 -0400

    Write basic Hyperapp.(app & h) bindings

--- src/hyperapp.ml
+++ src/hyperapp.ml
@@ -38,8 +38,12 @@ type vnode
 type ('state, 'actions) app_params =
   < state : 'state Js.t Js.undefined;
     view : 'state -> 'actions -> vnode;
-    actions : 'actions Js.t Js.undefined > Js.t
+    actions : 'actions Js.t Js.undefined;
+    root : Bs_webapi.Dom.Element.t > Js.t
 
+(**
+https://github.com/hyperapp/hyperapp/blob/f307aee3d14f0268660c277698c213d8e42cea8d/docs/api.md#h
+*)
 external h :
   string ->
   'attributes Js.t ->
@@ -50,9 +54,13 @@ external h :
 external _app : ('state, 'actions) app_params -> unit =
   "app" [@@bs.module "hyperapp"]
 
-let app ?state ?actions view = _app [%bs.obj {
-    state = Js.Undefined.from_opt state;
-    view;
-    actions = Js.Undefined.from_opt actions
-  }]
+(**
+https://github.com/hyperapp/hyperapp/blob/f307aee3d14f0268660c277698c213d8e42cea8d/docs/api.md#app
+*)
+let app ?state ~view ?actions root = _app [%bs.obj {
+  state = Js.Undefined.from_opt state;
+  view;
+  actions = Js.Undefined.from_opt actions;
+  root
+}]
 
--- src/index.ml
+++ src/index.ml
@@ -1,4 +1,9 @@
 let () =
-  Hyperapp.(app (fun _ _ ->
-    h "p" [%bs.obj { className = "main" }] (`text "Hello, Hyperapp!")))
+  let view _ _ =
+    Hyperapp.h
+      "p" [%bs.obj { className = "main" }] (`text "Hello, Hyperapp!") in
+
+  match Bs_webapi.Dom.(Document.getElementById "root" document) with
+    | Some root -> Hyperapp.app ~view root
+    | None -> ()
 
