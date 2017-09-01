26c7ef531f8b208192b2ace6d6f795cbaacfacf9
Yawar Amin <yawar.amin@mediative.com>
2017-09-01 10:51:26 -0400

    Add edit and set feature to counter example

--- src/index.ml
+++ src/index.ml
@@ -1,26 +1,36 @@
 open Hyperapp
 
-type msg = Increment | Decrement | Reset | Edit of string | Set
+type msg =
+  Increment | Decrement | Reset | Edit of int | Set | Disable of bool
 
-let initModel = 0, "0"
+(** Current value, edited value, 'set' button disabled or not *)
+let initModel = 0, 0, false
 
-let view (value, edit) msg =
+let view (value, edit, disabled) msg =
   let text = "Current value is: " ^ string_of_int value in
+  let onchange e =
+    let m = try Edit (int_of_string (valueOfEvent e))
+      with _ -> Disable true in
+
+    msg m in
+
+  let button ?(disabled=false) title m =
+    h_ "button" ~a:[%obj { disabled; onclick = fun _ -> msg m }] title in
 
   h "div" [
     h_ "p" ~a:[%obj { _class = "main" }] text;
-    h_ "button" ~a:[%obj { onclick = fun _ -> msg Increment }] "Increment";
-    h_ "button" ~a:[%obj { onclick = fun _ -> msg Decrement }] "Decrement";
-    h_ "button" ~a:[%obj { onclick = fun _ -> msg Reset }] "Reset";
-    h "input" ~a:[%obj { value = edit; onchange = fun e -> msg (Edit (valueOfEvent e)) }] [];
-    h_ "button" ~a:[%obj { onclick = fun _ -> msg Set }] "Set" ]
+    button "Increment" Increment;
+    button "Decrement" Decrement;
+    button "Reset" Reset;
+    h "input" ~a:[%obj { value = edit; onchange }] [];
+    button ~disabled "Set" Set ]
 
-let update ((value, edit) as model) = function
-  | Increment -> value + 1, edit
-  | Decrement -> value - 1, edit
+let update (value, edit, disabled) = function
+  | Increment -> value + 1, edit, disabled
+  | Decrement -> value - 1, edit, disabled
   | Reset -> initModel
-  | Edit edit -> value, edit
-  | Set -> try int_of_string edit, edit with  _ -> model
+  | Edit edit -> value, edit, false
+  | Set -> edit, edit, disabled
+  | Disable bool -> value, edit, bool
 
 let () = app ~model:initModel ~view ~update "root"
-
