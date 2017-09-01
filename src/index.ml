d2f61e7838ef91a65000737e54ba66a00c461a0f
Yawar Amin <yawar.amin@gmail.com>
2017-09-01 16:13:42 -0400

    Derive msg type constructor functions for ease of use

--- src/index.ml
+++ src/index.ml
@@ -2,35 +2,36 @@ open Hyperapp
 
 type msg =
   Increment | Decrement | Reset | Edit of int | Set | Disable of bool
+  [@@bs.deriving { accessors }]
 
-(** Current value, edited value, 'set' button disabled or not *)
+(** Current value, new value, whether 'set' button disabled *)
 let initModel = 0, 0, false
 
-let view (value, edit, disabled) msg =
+let view (value, newValue, disabled) msg =
   let text = "Current value is: " ^ string_of_int value in
   let onchange e =
-    let m = try Edit (int_of_string (valueOfEvent e))
-      with _ -> Disable true in
+    let m = try e |> valueOfEvent |> int_of_string |> edit with
+      | _ -> disable true in
 
     msg m in
 
   let button ?(disabled=false) title m =
     h_ "button" ~a:[%obj { disabled; onclick = fun _ -> msg m }] title in
 
-  h "div" [
-    h_ "p" ~a:[%obj { _class = "main" }] text;
-    button "Increment" Increment;
-    button "Decrement" Decrement;
-    button "Reset" Reset;
-    h "input" ~a:[%obj { value = edit; onchange }] [];
-    button ~disabled "Set" Set ]
+  h "div" ~a:[%obj { _class = "main" }] [
+    h_ "p" text;
+    button "Increment" increment;
+    button "Decrement" decrement;
+    button "Reset" reset;
+    h "input" ~a:[%obj { value = newValue; onchange }] [];
+    button ~disabled "Set" set ]
 
-let update (value, edit, disabled) = function
-  | Increment -> value + 1, edit, disabled
-  | Decrement -> value - 1, edit, disabled
+let update (value, newValue, disabled) = function
+  | Increment -> value + 1, newValue, disabled
+  | Decrement -> value - 1, newValue, disabled
   | Reset -> initModel
-  | Edit edit -> value, edit, false
-  | Set -> edit, edit, disabled
-  | Disable bool -> value, edit, bool
+  | Edit newValue -> value, newValue, false
+  | Set -> newValue, newValue, disabled
+  | Disable bool -> value, newValue, bool
 
 let () = app ~model:initModel ~view ~update "root"
